e16312b69958d5eeac9b1096a84b14ee6b1fa96d
yeahs2010@hotmail.com.tw <yeahs2010@hotmail.com.tw>
2018-06-10 15:58:01 +0800

    測試output

--- src/app/show-data/show-data.component.spec.ts
+++ src/app/show-data/show-data.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ShowDataComponent } from './show-data.component';
 import { of } from 'rxjs';
+import { Component, ViewChild } from '@angular/core';
 
 describe('ShowDataComponent', () => {
   let component: ShowDataComponent;
@@ -44,23 +45,63 @@ describe('ShowDataComponent', () => {
     httpController.verify(); // 確保上面都有跑到 可放在afterEach裡
   });
 
-  it('use spyOn',()=>{
+  it('use spyOn', () => {
     const mockResult = { name: 'Kevin' };
     const expectResult = 'Kevin!!';
 
     spyOn(component['http'], 'get').and.returnValue(of(mockResult));
 
-    component.getData().subscribe(value=>{
+    component.getData().subscribe(value => {
       expect(component['http'].get)
-      .toHaveBeenCalledWith('url');
+        .toHaveBeenCalledWith('url');
       expect(value).toEqual(expectResult);
     })
   })
 
-  it('use spyObject', ()=>{
+  it('use spyObject', () => {
     // TestingModule -> providers ->
     // {
     //   provide: HttpClient, useValue:httpClientSpyObject
     // }
   })
+
+  it('use output something', ()=>{
+    component.go.subscribe(value=>{
+      expect(value).toBe(true);
+    });
+    component.somethingClick();
+  })
 });
+
+
+
+
+// input
+// @Component({
+//   template: `<app-show-data [data]="mockData"></app-show-data>`
+// })
+// export class TestHostComponent {
+//   @ViewChild(ShowDataComponent) child;
+//   mockData;
+// }
+// describe('ShowDataComponent input output', () => {
+//   let fixture: ComponentFixture<TestHostComponent>;
+//   let component: TestHostComponent;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         ShowDataComponent,
+//         TestHostComponent
+//       ]
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(TestHostComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should have data input', () => {
+
+//   })
+// })
--- src/app/show-data/show-data.component.ts
+++ src/app/show-data/show-data.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-show-data',
   templateUrl: './show-data.component.html',
   styleUrls: ['./show-data.component.css']
 })
 export class ShowDataComponent implements OnInit {
-
+@Output() go = new EventEmitter<boolean>()
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+
   getData() {
     return this.http.get('url').pipe(map(
       (value: any) => value.name + '!!'
     ));
+  }
+
+  somethingClick(){
+    this.go.emit(true);
   }
 }
